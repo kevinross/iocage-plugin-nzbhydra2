e6c8ba41e7be30279c0fd8c87c3ea09381711f31
Stefan Schramek <stefanschramek@users.noreply.github.com>
2019-05-19 20:06:31 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 # Fetch latest release
-fetch $(curl -s https://api.github.com/repos/theotherp/nzbhydra2/releases/latest | grep browser_download_url | grep 'linux[.]zip' | cut -d '"' -f 4) -o /usr/local/share
+fetch $(curl -s https://api.github.com/repos/theotherp/nzbhydra2/releases/latest | grep browser_download_url | grep 'linux[.]zip' | cut -d '"' -f 4) -o /usr/local/
 
 # Extract
-unzip /usr/local/share/nzbhydra2-*-linux.zip -o -d /usr/local/share/nzbhydra2
+unzip /usr/local/nzbhydra2-*-linux.zip -o -d /usr/local/nzbhydra2
+
+#Remove
+rm /usr/local/nzbhydra2-*-linux.zip
 
 # Add user
 pw user add nzbhydra2 -c nzbhydra2 -u 999 -d /nonexistent -s /usr/bin/nologin
 
 # Add service
-cp /usr/local/share/nzbhydra2/rc.d/nzbhydra2 /etc/rc.d/nzbhydra2
+cp /usr/local/nzbhydra2/rc.d/nzbhydra2 /etc/rc.d/nzbhydra2
 chmod u+x /etc/rc.d/nzbhydra2
 
 # Enable the service
