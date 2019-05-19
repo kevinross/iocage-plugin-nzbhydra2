19d1662b616167cc666db4064edc19582426a827
Stefan Schramek <stefanschramek@users.noreply.github.com>
2019-05-19 19:56:59 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -10,8 +10,8 @@ unzip /usr/local/share/nzbhydra2-*-linux.zip -o -d /usr/local/share/nzbhydra2
 pw user add nzbhydra2 -c nzbhydra2 -u 999 -d /nonexistent -s /usr/bin/nologin
 
 # Add service
-cp /usr/local/share/nzbhydra2/rc.d/nzbhydra2 /usr/local/etc/rc.d/nzbhydra2
-chmod u+x /usr/local/etc/rc.d/nzbhydra2
+cp /usr/local/share/nzbhydra2/rc.d/nzbhydra2 /etc/rc.d/nzbhydra2
+chmod u+x /etc/rc.d/nzbhydra2
 
 # Enable the service
 sysrc -f /etc/rc.conf nzbhydra2_enable="YES"
